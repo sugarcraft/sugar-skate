167baf75caa30cf47b30b16d3337874a8c0cdf69
Joe Huss <detain@interserver.net>
2026-05-07 06:00:16 -0400

    Add Dutch locale for all libs (#259)

---
+++ lang/nl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Dutch translations for sugar-skate.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'database.entry_unreadable' => 'Entry ingesteld maar onleesbaar',
+    'store.cannot_read'         => 'Kan bestand niet lezen: {path}',
+    'cli.usage_set'             => 'Gebruik: {bin} set <key> [waarde]',
+    'cli.usage_get'             => 'Gebruik: {bin} get <key>',
+    'cli.usage_delete'          => 'Gebruik: {bin} delete <key>',
+    'cli.deleted_n'             => '{count} entries verwijderd.',
+    'cli.unknown_command'       => 'Onbekend commando: {cmd}',
+];
