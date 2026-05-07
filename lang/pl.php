6d19078537874f71f8b092fbfb2c4a977ef83842
Joe Huss <detain@interserver.net>
2026-05-07 05:58:30 -0400

    Add Polish locale for all libs (#258)

---
+++ lang/pl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Polish translations for sugar-skate.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'database.entry_unreadable' => 'Wpis ustawiony, ale nieczytelny',
+    'store.cannot_read'         => 'Nie można odczytać pliku: {path}',
+    'cli.usage_set'             => 'Użycie: {bin} set <klucz> [wartość]',
+    'cli.usage_get'             => 'Użycie: {bin} get <klucz>',
+    'cli.usage_delete'          => 'Użycie: {bin} delete <klucz>',
+    'cli.deleted_n'             => 'Usunięto {count} wpisów.',
+    'cli.unknown_command'       => 'Nieznane polecenie: {cmd}',
+];
